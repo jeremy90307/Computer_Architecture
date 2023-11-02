d846a010a73da62ef04831c7d7c8042b826b77b2
jeremytsai <jeremy90307@gmail.com>
2023-11-02 23:50:12 +0800

    20231102

--- HW2/lab2.c
+++ HW2/lab2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<math.h>
+#include <inttypes.h>
 
 # define array_size 7
 # define range 127 /*2^(n-1)-1, n: quant bit*/ 
@@ -8,9 +9,26 @@
 float fp32_to_bf16(float x);
 int* quant_bf16_to_int8(float x[]);
 float bf16_findmax(float x[]);
-
+typedef uint64_t ticks;
+static inline ticks getticks(void)
+{
+    uint64_t result;
+    uint32_t l, h, h2;
+    asm volatile(
+        "rdcycleh %0\n"
+        "rdcycle %1\n"
+        "rdcycleh %2\n"
+        "sub %0, %0, %2\n"
+        "seqz %0, %0\n"
+        "sub %0, zero, %0\n"
+        "and %1, %1, %0\n"
+        : "=r"(h), "=r"(l), "=r"(h2));
+    result = (((uint64_t) h) << 32) | ((uint64_t) l);
+    return result;
+}
 int main()
 {
+	ticks t0 = getticks();
 	float array[array_size] = {1.200000, 1.203125, 2.310000, 2.312500, 3.460000, 3.4531255, 5.630000};
 	float array2[array_size] = { 0.1, 0.2, 1.2, 3, 2.1, -4.2, 3.5};
 	float array3[array_size] = { 3.14159265, 0.12345678 , 1.23456789 , 0.00000123, 0.00000001, 0.99999999 , 0.00000007 };
@@ -55,6 +73,8 @@ int main()
 	for (int i = 0; i < array_size; i++) {
 		printf("%d\n", after_quant[i]);
 	}
+	ticks t1 = getticks();
+    printf("elapsed cycle: %" PRIu64 "\n", t1 - t0);
 	system("pause");
 	return 0;
 }
